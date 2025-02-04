965cedca5d4506edbf0f24ca34d133d49d031a7f
Sadykov Arsen <LoCo10291998@gmail.com>
2025-02-04 16:48:46 +0600

    Commit. I built the path to the required components using the Routes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contacts from "./components/Contacts";
 
-const App = () => {
-
+function App() {
   return (
-    <>
+      <Router>
+        <Navbar />
+        <div className="container mt-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contacts" element={<Contacts />} />
+          </Routes>
+        </div>
+      </Router>
+  );
+}
 
-    </>
-  )
-};
-
-export default App
+export default App;
