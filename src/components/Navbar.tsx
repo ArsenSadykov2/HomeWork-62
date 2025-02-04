f32e0e49c7d247d372e67d67e92c6e01504032b5
Sadykov Arsen <LoCo10291998@gmail.com>
2025-02-04 17:33:20 +0600

    Commit. Created a component for NavBar. Where are the links to the following pages. And created the Home page. Where basic information on this topic is provided. And to everything else I added some styles.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,32 @@
+import { NavLink } from "react-router-dom";
 
-const Navbar = () => {
-    return (
-        <div>
-
+const Navbar = () => (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+            <NavLink to="/" className="navbar-brand">
+                Акатцуки
+            </NavLink>
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav me-auto">
+                    <li className="nav-item">
+                        <NavLink to="/" className="nav-link">
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/about" className="nav-link">
+                            About Us
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/contacts" className="nav-link">
+                            Contacts
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
         </div>
-    );
-};
+    </nav>
+);
 
 export default Navbar;
