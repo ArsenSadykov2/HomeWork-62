04eeb66cc9e26122524159510ab59148a31da6cb
Sadykov Arsen <LoCo10291998@gmail.com>
2025-02-04 18:06:20 +0600

    Commit. Created the Contacts component. Where I indicated information on establishing contact with the organizer of the Group. And left some information about him. And added some style.

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,11 +1,33 @@
+const Contacts = () => (
+    <div className="contacts-container">
+        <h1 className="title-contacts">Контакты Акатцуки</h1>
+        <div className="contact-info">
+            <p className="description">
+                Акатцуки — организация, которая скрывается в тени и выполняет опасные миссии. Для связи с лидером
+                Акатцуки, Пейном, вам нужно будет проявить терпение и осторожность, так как он редко покидает
+                Амегакуре, свою базу в Деревне Дождя.
+            </p>
 
-
-const Contacts = () => {
-    return (
-        <div>
-            
+            <ul className="contact-list">
+                <li>
+                    <strong>Лидер:</strong> Пейн (Нагато)
+                </li>
+                <li>
+                    <strong>Деревня:</strong> Амегакуре (Деревня Дождя)
+                </li>
+                <li>
+                    <strong>Связь:</strong> Найдите его в Амегакуре, но будьте осторожны! Лидер не доверяет никому.
+                </li>
+            </ul>
         </div>
-    );
-};
+
+        <div className="contact-quote">
+            <blockquote>
+                <em>"Долгие и мрачные пути ведут к силе. Но они требуют преданности и решимости."</em>
+                — Пейн (Нагато)
+            </blockquote>
+        </div>
+    </div>
+);
 
 export default Contacts;
